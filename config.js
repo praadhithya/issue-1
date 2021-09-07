787cf5fb7a73693bc9f13771aad676da9c20326b
praadhithya <59948300+praadhithya@users.noreply.github.com>
2021-09-07 17:47:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,1 @@
-export
-const firebaseConfig = {
-    apiKey: "AIzaSyBOEsqJfFr-iWAlgnXwgqy2IrR2vEQ7u2U",
-    authDomain: "helecopter-b4561.firebaseapp.com",
-    databaseURL: "https://helecopter-b4561-default-rtdb.firebaseio.com",
-    projectId: "helecopter-b4561",
-    storageBucket: "helecopter-b4561.appspot.com",
-    messagingSenderId: "659659605335",
-    appId: "1:659659605335:web:f8a79946e495050819ea12",
-    measurementId: "G-XVQLBECB20"
-  };
+
